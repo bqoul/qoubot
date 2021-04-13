af85e3c747f85b48f94560e873e059e7a4ce4815
bqoul <mbqoul@gmail.com>
2021-04-13 19:57:24 +0300

    creating client.js file and setting up commands and message listeners

--- bot.js
+++ bot.js
@@ -1,16 +1,84 @@
-const tmi = require('tmi.js');
-const fs = require('fs');
+const c = require('./client');
 
-const oauth = JSON.parse(fs.readFileSync('oauth.json'));
-const channels = JSON.parse(fs.readFileSync('channels.json'));
-const client = new tmi.Client({
-    options: {debug: true},
-    connection: {reconnect: true},
-    identity: {
-        username: 'qoubot',
-        password: oauth.token,
-    },
-    channels: channels.list,
+c.client.connect();
+
+//global timeout protection
+let waiting = false;
+let waiting_channels = [];
+
+const sleep = async (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const gtp = async (channel) => {
+    let pos = waiting_channels.push(channel) - 1; //pushing channel in and getting its position
+    waiting = true;                               //so i can disable command listener only for channel in which command was used
+    await sleep(1500);
+    waiting_channels.splice(pos);
+    waiting = false;
+}
+
+c.client.on('message', async (channel, user, message, self) => { //command listeners
+    if (waiting_channels.includes(channel) && waiting || self) {
+        return;
+    }
+
+    switch(message.split(' ')[0]) {
+        case '&help':
+            c.client.say(channel, `@${user.username} no elp NOPERS`);
+            gtp(channel);
+            break;
+
+        case '&commands':
+            c.client.say(channel, `@${user.username} you can find all commands here => https://github.com/bqoul/qoubot`);
+            gtp(channel);
+            break;
+
+        case '&repeat':
+            gtp(channel);
+            break;
+
+        case '&repeatshuffle':
+            gtp(channel);
+            break;
+
+        case '&stop':
+            gtp(channel);
+            break;
+
+        case '&weather':
+            gtp(channel);
+            break;
+
+        case '&pyramid':
+            gtp(channel);
+            break;
+
+        case '&shuffle':
+            gtp(channel);
+            break;
+
+        case '&vanish':
+            gtp(channel);
+            break;
+
+        case '&gay':
+            gtp(channel);
+            break;
+
+        case '&iq':
+            gtp(channel);
+            break;
+
+        case '&quiz':
+            gtp(channel);
+            break;
+
+    }
 });
 
-client.connect();
+c.client.on('message', async (channel, user, message, self) => { //message listeners
+    if (self) {
+        return;
+    }
+})
