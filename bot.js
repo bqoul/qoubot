0499efb18d34993c842c68f5949c8c09ab550885
bqoul <mbqoul@gmail.com>
2021-04-13 19:21:44 +0300

    starting up

--- bot.js
+++ bot.js
@@ -1,1 +1,16 @@
-console.log('hello world!');
+const tmi = require('tmi.js');
+const fs = require('fs');
+
+const oauth = JSON.parse(fs.readFileSync('oauth.json'));
+const channels = JSON.parse(fs.readFileSync('channels.json'));
+const client = new tmi.Client({
+    options: {debug: true},
+    connection: {reconnect: true},
+    identity: {
+        username: 'qoubot',
+        password: oauth.token,
+    },
+    channels: channels.list,
+});
+
+client.connect();
