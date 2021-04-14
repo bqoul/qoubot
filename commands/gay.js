ff226505f8fe2454aef9ed0289fe8c76c3f4ea38
bqoul <mbqoul@gmail.com>
2021-04-14 15:48:14 +0300

    removed something Kappa

--- commands/gay.js
+++ commands/gay.js
@@ -1,7 +1,7 @@
 const client = require('../client');
 
 const gay = (channel, user, message) => {
-    let persent = Math.floor(Math.random() * (100 - -1 + 1)) + -1;
+    let persent = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
     let target = message.split(' ')[1];
 
     if (target === undefined) {
@@ -9,16 +9,12 @@ const gay = (channel, user, message) => {
             client.say(channel, `@${user.username} you are ${persent}% gay Kappa`);
         } else if (persent > 50) {
             client.say(channel, `@${user.username} you are ${persent}% gay KappaPride`);
-        } else if (persent == -1) {
-            client.say(channel, `@${user.username} you are a̷͂̌s̴̛͠%̴̈́̓ gay AppaK`);
         }
     } else {
         if (persent < 50) {
             client.say(channel, `${target} are ${persent}% gay Kappa`);
         } else if (persent > 50) {
             client.say(channel, `${target} are ${persent}% gay KappaPride`);
-        } else if (persent == -1) {
-            client.say(channel, `${target} are a̷͂̌s̴̛͠%̴̈́̓ gay AppaK`);
         }
     }
 }
