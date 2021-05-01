4a7dc7d45c3f7a2f047d6e6336867df57a80b956
bqoul <mbqoul@gmail.com>
2021-05-01 18:10:09 +0300

    fixed &stop bug

--- commands/stop.js
+++ commands/stop.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const client = require('../client');
 
 const stop = (channel, user) => {
-    let repeat_data = JSON.parse(fs.readFileSync('data/repeat.json'));
+    let repeat_data = JSON.parse(fs.readFileSync(`data/repeat/${channel}.json`));
     
     repeat_data[channel].target = '';
 
     client.say(channel, `@${user.username} ok sorry Sadge`);
-    fs.writeFileSync('data/repeat.json', JSON.stringify(repeat_data, null, 1));
+    fs.writeFileSync(`data/repeat/${channel}.json`, JSON.stringify(repeat_data, null, 1));
 }
 
 module.exports = stop;
