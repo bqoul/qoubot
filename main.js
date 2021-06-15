4c3b974f4d092ff3749474d4a1f88775a9fb2358
bqoul <mbqoul@gmail.com>
2021-06-15 21:15:19 +0300

    now if website crashes the bot will reconnect to the channels immediately

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const aliases = require("./aliases");
+const Bot = require("./bot");
 
-aliases.data.connect();
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 	console.log(`listening to port ${PORT}...`);
+	aliases.data.connect();
+	/*
+		getting list of connected channels from database
+		to connect the bot to this channels when website starts
+		(in case if website crashes or something)
+	*/
+	const connected = require("./aliases/data/connected");
+	const channel_list = await connected.find();
+
+	let bot = {};
+	for(const {channel} of channel_list) {
+		bot[channel] = new Bot(channel);
+		bot[channel].connect();
+	}
 });
 
 app.get("/", (req, res) => {
