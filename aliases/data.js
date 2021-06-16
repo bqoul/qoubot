d550466b0a7d4424a85d7478a948b829d707400f
bqoul <mbqoul@gmail.com>
2021-06-16 14:25:15 +0300

    renamed few variables

--- aliases/data.js
+++ aliases/data.js
@@ -6,29 +6,29 @@ module.exports = {
 		mongoose.connect(`mongodb+srv://bqoul:${process.env.DATABASE_PASSWORD}@data.sqprg.mongodb.net/Data?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true,});
 	},
 	async get(type, channel) {
-		const data = require(`./data/${type}`);
-		return data.findOne({channel: channel});
+		const schema = require(`./schemas/${type}`);
+		return schema.findOne({channel: channel});
 	},
 	async get_all(type) {
-		const data = require(`./data/${type}`);
-		return data.find();
+		const schema = require(`./schemas/${type}`);
+		return schema.find();
 	},
 	async set(type, options) {
-		const data = require(`./data/${type}`);
+		const schema = require(`./schemas/${type}`);
 		const get = await this.get(type, options.channel);
 		//check if document with this data already exists
 		if(get) {
 			//if so, updating it with new data
-			await data.updateOne({
+			await schema.updateOne({
 				channel: options.channel,
 			}, options);
 			return;
 		}
 		//if not, creating new document
-		await new data(options).save();
+		await new schema(options).save();
 	},
 	async delete(type, channel) {
-		const data = require(`./data/${type}`);
-		await data.deleteOne({channel: channel});
+		const schema = require(`./schemas/${type}`);
+		await schema.deleteOne({channel: channel});
 	},
 }
