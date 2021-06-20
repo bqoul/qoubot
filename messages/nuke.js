7a9758ad0e0566064f964295d4d15420cc872302
bqoul <mbqoul@gmail.com>
2021-06-20 17:04:41 +0300

    comment

--- messages/nuke.js
+++ messages/nuke.js
@@ -7,7 +7,7 @@ module.exports = {
 
 		const {nukes} = await aliases.data.get("nuke", params.channel);
 		for(const [phrase, duration] of Object.entries(nukes)) {
-			//TODO: fix bug with single emote phrases (bot timeouting every second time for some reason)
+			//TODO: fix bug with single twitch emote phrases (bot timeouting every second time for some reason)
 			if(params.message.toLowerCase().includes(phrase.toLowerCase())) {
 				params.bot.timeout(params.channel, params.user.username, duration, "nuked");
 				return;
