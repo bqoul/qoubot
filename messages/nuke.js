db6d2c3c22298a8efbfc466da33638fae127543b
bqoul <mbqoul@gmail.com>
2021-06-20 17:16:22 +0300

    comment

--- messages/nuke.js
+++ messages/nuke.js
@@ -4,8 +4,8 @@ module.exports = {
 	run: async (params) => {
 		const role = aliases.role(params.user);
 		if(role === "streamer" || role === "mod" || role === "vip") return;
-
-		const {nukes} = await aliases.data.get("nuke", params.channel);
+		//if nukes is empty for this channel, adding dummy object to prevent runtime errors
+		const {nukes} = await aliases.data.get("nuke", params.channel) ?? {nukes: {}};
 		for(const [phrase, duration] of Object.entries(nukes)) {
 			//TODO: fix bug with single twitch emote phrases (bot timeouting every second time for some reason)
 			if(params.message.toLowerCase().includes(phrase.toLowerCase())) {
