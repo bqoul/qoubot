100680912fdce1d102fedafb1490c26503793f9f
bqoul <mbqoul@gmail.com>
2021-06-20 12:06:03 +0300

    slight tweaks, nothing important

--- bot.js
+++ bot.js
@@ -5,15 +5,15 @@ const aliases = require("./aliases");
 module.exports = class Bot {
 	constructor(channel) {
 		//creating new bot in an object for every chat to connect / disconnect qoubot from the channel on call
-		this[channel] = twitch.bot(channel);
-		//and saving the channel name for simpler access to the bot
+		this.bot[channel] = twitch.bot(channel);
+		//and saving the channel name for easier access to the bot
 		this.channel = channel;
 	}
 	//empty array for channels, to avoid global timeout
 	waiting = [];
 	connect() {
-		this[this.channel].connect();
-		this[this.channel].on("message", async (channel, user, message, self) => {
+		this.bot[this.channel].connect();
+		this.bot[this.channel].on("message", async (channel, user, message, self) => {
 			if(self || this.waiting.includes(channel)) return;
 
 			//looking for custom index in database, if theres no custom index - setting it to default value
@@ -26,7 +26,7 @@ module.exports = class Bot {
 					if(`${index}${tag}`.toLowerCase() == message.split(" ")[0].toLowerCase() && (!command.roles || command.roles.includes(aliases.role(user)))) {
 						// tag.run(this[this.channel], channel, user, message);
 						command.run({
-							bot: this[this.channel],
+							bot: this.bot[this.channel],
 							channel: channel,
 							user: user,
 							message: message,
@@ -40,6 +40,6 @@ module.exports = class Bot {
 		});
 	}
 	disconnect() {
-		this[this.channel].disconnect();
+		this.bot[this.channel].disconnect();
 	}
 }
