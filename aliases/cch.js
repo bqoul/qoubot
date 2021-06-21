2c6e19fda4c0cf2e071c8b99caec1ef7bba57425
bqoul <mbqoul@gmail.com>
2021-06-21 22:27:04 +0300

    added {rnd&v} tag to the custom command constructor

--- aliases/cch.js
+++ aliases/cch.js
@@ -1,9 +1,10 @@
+const fetch = require("node-fetch");
 //custom command handler
-module.exports = (params) => {
+module.exports = async (params) => {
 	//adding one to the amount for calls for the data
 	params.command.calls += 1;
 	//recursive function to format all the tags
-	const format = (text) => {
+	const format = async (text) => {
 		let substr = text.substring(text.lastIndexOf("{") + 1, text.lastIndexOf("}"));
 		switch(substr.split("&")[0]) {
 			case "user":
@@ -15,6 +16,17 @@ module.exports = (params) => {
 			case "rnd":
 				const options = substr.split("&")[1];
 				switch(options) {
+					//expected tag -> {rnd&v}
+					case "v":
+						//this works little slow but it works!
+						const request = await fetch(`https://tmi.twitch.tv/group/user/${params.channel.slice(1)}/chatters`);
+						const responce = await request.json();
+
+						const viewer = ~~(Math.random() * ((responce.chatters.viewers.length - 1) - 0 + 1)) + 0;
+
+						return format(text.replace(`{${substr}}`, responce.chatters.viewers[viewer]));
+
+					//expected tag -> {rnd&min-max}
 					default: 
 						const min = parseInt(options.split("-")[0]);
 						const max = parseInt(options.split("-")[1]);
@@ -28,5 +40,5 @@ module.exports = (params) => {
 		}
 	}
 
-	params.bot.say(params.channel, format(params.command.text));
+	params.bot.say(params.channel, await format(params.command.text));
 }
